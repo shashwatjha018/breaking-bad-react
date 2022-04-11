424c061e5958cb8c9c6f48c0d2582155bc2bd9ef
shashwatjha018 <shashwat.jha2019@vitstudent.ac.in>
2022-04-11 23:25:41 +0530

    Updates

---
+++ precache-manifest.8d4f947bd1bbe039e245605f09e991b2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc1eea70248e8740d8d8dc0b35adf3fd",
+    "url": "/breaking-bad-react/index.html"
+  },
+  {
+    "revision": "dd8a1ee8ede10c13a3a0",
+    "url": "/breaking-bad-react/static/css/main.25f97c37.chunk.css"
+  },
+  {
+    "revision": "c9413a2b0e288bcb1e85",
+    "url": "/breaking-bad-react/static/js/2.68f4301c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/breaking-bad-react/static/js/2.68f4301c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd8a1ee8ede10c13a3a0",
+    "url": "/breaking-bad-react/static/js/main.0422dc95.chunk.js"
+  },
+  {
+    "revision": "45b492dce5227312a078",
+    "url": "/breaking-bad-react/static/js/runtime-main.c1b16971.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/breaking-bad-react/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/breaking-bad-react/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/breaking-bad-react/static/media/spinner.235b9b3e.gif"
+  }
+]);
